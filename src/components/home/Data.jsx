5d2dcc7c9eefa5a89d73193ffc24693f2e1714be
rubuz <rubuz23@gmail.com>
2023-04-11 14:45:33 +0200

    home screen semi done

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Matjaž Lovenjak</h1>
+      <h3 className="home__subtitle">Web Developer</h3>
+      <p className="home__description">
+        I'm self taught web developer with a passion for front-end, based in
+        Brežice, Slovenia
+      </p>
+      <a href="#contact" className="button button--flex">
+        Say Hello <i className="uil uil-message"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Data;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/rubuz88/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/matjaz-lovenjak/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+      <a
+        href="https://github.com/rubuz"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
