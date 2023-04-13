ec2c12dfc0a68377bd5a7890d34dc54fb89c8e3d
rubuz <rubuz23@gmail.com>
2023-04-13 12:02:46 +0200

    footer done

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Lovenjak</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className="footer__link">
+              Projects
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://www.instagram.com/rubuz88/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i className="uil uil-instagram"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/matjaz-lovenjak/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="uil uil-linkedin"></i>
+          </a>
+          <a
+            href="https://github.com/rubuz"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="uil uil-github"></i>
+          </a>
+        </div>
+        <span className="footer__copy">
+          &#169; Lovenjak. All rights reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
