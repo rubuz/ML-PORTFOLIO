40eea2f54bd52ae6b93385c479b5d276ac59a6a5
rubuz <rubuz23@gmail.com>
2023-04-14 11:53:54 +0200

    modal fixes

--- src/components/projects/ProjectItem.jsx
+++ src/components/projects/ProjectItem.jsx
@@ -9,12 +9,16 @@ const ProjectItem = ({ item }) => {
 
   return (
     <div className="project__card" key={item.id}>
-      <img
-        src={item.image}
-        alt="project small screenshot"
-        className="project__img"
-        onClick={() => toggleTab(true)}
-      />
+      <div className="project__img-container" onClick={() => toggleTab(true)}>
+        <img
+          src={item.image}
+          alt="project small screenshot"
+          className="project__img"
+        />
+        <div className="project__img-layer">
+          <p className="project__img-layer__text">View More</p>
+        </div>
+      </div>
       <h3 className="project__title">{item.title}</h3>
       <a href={item.url} className="project__button" target="_blank">
         Demo
