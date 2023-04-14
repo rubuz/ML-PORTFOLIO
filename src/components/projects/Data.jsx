a4a17cbc5d40d898f219b4df655ba4024f8f1587
rubuz <rubuz23@gmail.com>
2023-04-14 11:19:26 +0200

    modal done

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -10,29 +10,51 @@ export const projectsData = [
     image: Project1,
     title: "ntRoot",
     url: "https://eloquent-bonbon-b4a5f7.netlify.app",
+    modal: {
+      description: "Website for small bussnies.",
+      tech: ["HTML5", "CSS3", "Javascript"],
+    },
   },
   {
     id: 2,
     image: Project2,
     title: "Tenzies",
     url: "https://hilarious-semifreddo-72ccdd.netlify.app",
+    modal: {
+      description: "Small game made with ReactJS.",
+      tech: ["ReactJS"],
+    },
   },
   {
     id: 3,
     image: Project3,
     title: "Weather app",
     url: "https://peppy-platypus-d29492.netlify.app/",
+    modal: {
+      description:
+        "Website uses weather APIs to display current weather and forecast.",
+      tech: ["HTML5", "CSS3", "Javascript"],
+    },
   },
   {
     id: 4,
     image: Project4,
     title: "Tip Calculator",
     url: "https://jade-macaron-5eeb28.netlify.app/",
+    modal: {
+      description:
+        "Calculator with calculate a cost per person, based on bill size and chosen tip percentge.",
+      tech: ["HTML5", "CSS3", "Javascript"],
+    },
   },
   {
     id: 5,
     image: Project5,
     title: "TicTacToe",
     url: "https://inspiring-kelpie-c794ab.netlify.app/",
+    modal: {
+      description: "Simple TicTacToe game with names editor.",
+      tech: ["HTML5", "CSS3", "Javascript"],
+    },
   },
 ];
--- src/components/projects/ProjectItem.jsx
+++ src/components/projects/ProjectItem.jsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProjectItem = ({ item }) => {
+  const [toggleState, setToggleState] = useState(false);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <div className="project__card" key={item.id}>
       <img
         src={item.image}
         alt="project small screenshot"
         className="project__img"
+        onClick={() => toggleTab(true)}
       />
       <h3 className="project__title">{item.title}</h3>
       <a href={item.url} className="project__button" target="_blank">
         Demo
         <i class="bx bx-chevron-right project__button-icon"></i>
       </a>
+
+      <div
+        className={
+          toggleState ? "project__modal active__modal" : "project__modal"
+        }
+      >
+        <div className="project__modal-content">
+          <h3>{item.title}</h3>
+          <p className="project__modal-content__description">
+            {item.modal.description}
+          </p>
+          <div className="project__modal-content-tech">
+            <p className="tech-title">Stack:</p>
+            <ul>
+              {item.modal.tech.map((tech) => (
+                <li>{tech}</li>
+              ))}
+            </ul>
+          </div>
+          <i
+            class="uil uil-times project__modal-close"
+            onClick={() => toggleTab(false)}
+          ></i>
+          <a href={item.url} className="project__button" target="_blank">
+            Demo
+            <i class="bx bx-chevron-right project__button-icon"></i>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
