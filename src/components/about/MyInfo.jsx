2857ce2c1af79475f85319a594160140650962b1
rubuz <rubuz23@gmail.com>
2023-07-12 12:52:43 +0200

    fix

--- src/components/about/MyInfo.jsx
+++ src/components/about/MyInfo.jsx
@@ -6,7 +6,7 @@ const MyInfo = () => {
       <div className="about__box">
         <i className="bx bx-laptop about__icon"></i>
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">6 Years Working in IT</span>
+        <span className="about__subtitle">7 Years Working in IT</span>
       </div>
       <div className="about__box">
         <i className="bx bx-user-pin about__icon"></i>
